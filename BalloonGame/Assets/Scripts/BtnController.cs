1764bc33ca94a24c595874cc7897b4258852b893
Honey Jain <hrjain911@gmail.com>
2021-08-10 18:23:43 +0530

    Added Environment Background, PlayButton, PlayScene

---
+++ BalloonGame/Assets/Scripts/BtnController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BtnController : MonoBehaviour
+{
+
+    public Button btnPlay;
+    public string sceneName;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnPlay.onClick.AddListener(PlayGame);
+    }
+
+    private void PlayGame()
+    {
+        Debug.Log("Play Button Clicked");
+        SceneManager.LoadScene(sceneName);
+    }
+}
